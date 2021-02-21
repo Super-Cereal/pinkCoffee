6a72ac379bc4f586874ee73f80d6d86d4960e81e
Super-Cereal <Super-Cereal>
2021-02-21 18:29:38 +0200

    base

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import path from "path";
 import ejs from "ejs";
 
+import data from "data.js"
+
 const __dirname = path.resolve();
 const PORT = process.env.PORT ?? 8080;
 const app = express();
@@ -10,12 +12,15 @@ const renderTemplate = (alias, data) => {
   let filename = path.resolve(__dirname, "templates", "index.ejs");
   let dataForRender = {
     alias: alias,
+    data: data,
   };
   return ejs.renderFile(filename, dataForRender);
 };
 
 app.get("/", (req, res) => {
-  renderTemplate("Leaders", "").then((html) => {
+  // let theme = req.query.theme ?? "dark"
+  let slide = req.query.slide - 1 ?? 0
+  renderTemplate(data[slide].alias, data[slide].data).then((html) => {
     res.send(html);
   });
 });
