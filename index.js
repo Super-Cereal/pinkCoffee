e8ae1c023d2183751ce338cc0d7fd12045e9fe88
Egor <bifrost.step@gmail.com>
2021-02-22 13:12:58 +0300

    first_template v0.1

--- index.js
+++ index.js
@@ -2,24 +2,21 @@ import express from "express";
 import path from "path";
 import ejs from "ejs";
 
-import data from "data.js"
+import data from "./data.js";
 
 const __dirname = path.resolve();
 const PORT = process.env.PORT ?? 8080;
 const app = express();
+app.use(express.static(__dirname + '/public'));
 
 const renderTemplate = (alias, data) => {
   let filename = path.resolve(__dirname, "templates", "index.ejs");
-  let dataForRender = {
-    alias: alias,
-    data: data,
-  };
-  return ejs.renderFile(filename, dataForRender);
+  return ejs.renderFile(filename, { alias, data });
 };
 
 app.get("/", (req, res) => {
   // let theme = req.query.theme ?? "dark"
-  let slide = req.query.slide - 1 ?? 0
+  let slide = (req.query.slide ?? 1) - 1;
   renderTemplate(data[slide].alias, data[slide].data).then((html) => {
     res.send(html);
   });
